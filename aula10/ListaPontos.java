8a1445fb5b678bae2063185566bd97f616ba4920
amignon <amignon@gmail.com>
2025-10-31 13:32:16 +0000

    Adição da ListaPontos

---
+++ aula10/ListaPontos.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+public class ListaPontos {
+    ArrayList<Ponto> pontos;
+
+    public ListaPontos() {
+        pontos = new ArrayList<Ponto>();
+    }
+
+    public void adicionaPonto(Ponto p) {
+        pontos.add(p);
+    }
+
+    public ArrayList<Ponto> pontosProximos(Circulo c) {
+        ArrayList<Ponto> proximos = new ArrayList<>();
+        for (Ponto p : pontos) {
+            if (c.estaContido(p)) {
+                proximos.add(p);
+            }
+        }
+        return proximos;
+    }
+}
